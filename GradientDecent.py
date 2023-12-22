ecf4bdbc2fdda03a2f1ae9dc8201d5af893e64f5
Ashutosh Mishra <101855140+CosmoAshutosh@users.noreply.github.com>
2023-12-22 11:46:30 +0530

    Update GradientDecent.py
    Optimized gradient decent

--- GradientDecent.py
+++ GradientDecent.py
@@ -1,50 +1,36 @@
 import numpy as np
-import math
-from sympy import *
-import numdifftools as nd
-
+from sympy import Symbol, diff, lambdify
 
 def gradient(fcn, var, var_x, var_y):
-     x = Symbol('x')
-     y = Symbol('y')
-
-     df = diff(fcn, var)
-     df = lambdify([x,y], df)
-     return df(var_x, var_y)
+    x, y = Symbol('x'), Symbol('y')
+    df = diff(fcn, var)
+    df = lambdify([x, y], df)
+    return df(var_x, var_y)
 
 def function_value(fcn, varx, vary):
-     x = Symbol('x')
-     y = Symbol('y')
-     fcn = lambdify([x,y], fcn)
+    x, y = Symbol('x'), Symbol('y')
+    fcn = lambdify([x, y], fcn)
+    return fcn(varx, vary)
 
-     return fcn(varx, vary)
+def gradient_descent(function, learning_rate, iteration, x_start, y_start):
+    x, y = Symbol('x'), Symbol('y')
+    X, Y = x_start, y_start
 
-def gradient_decent(function, learning_rate, iteration, x_start, y_start):
-     X = x_start
-     Y = y_start
-     x = Symbol('x')
-     y = Symbol('y')
-     print("Our learning rate is :", learningRate)
-     print("Our number of iterations is :", iteration)
+    for _ in range(iteration):
+        grad_x = gradient(function, x, X, Y)
+        grad_y = gradient(function, y, X, Y)
 
-     for i in range(1, iteration+1):
-          grad_x = gradient(function, x, X, Y)
-          grad_y = gradient(function, y, X, Y)
+        X -= learning_rate * grad_x
+        Y -= learning_rate * grad_y
 
-          new_X = X - (learning_rate*grad_x)
-          X = new_X
-          new_Y = Y - (learning_rate*grad_y)
-          Y = new_Y
-
-          print("New points in ",i ,"iteration is ", X, Y)
-          
-     print("Optimal points are : ", X, Y ,"and optimal value of the function is : ", function_value(function, X, Y))
+    optimal_value = function_value(function, X, Y)
+    return X, Y, optimal_value
 
 input_expr = input('Enter an expression: ')
-learningRate = float(input('Enter the learning rate: '))
-Iteration = int(input('Enter number of iteration: '))
-X_Start = int(input('Enter starting point of x: '))
-Y_Start = int(input('Enter starting point of y: '))
+learning_rate = float(input('Enter the learning rate: '))
+iteration = int(input('Enter number of iterations: '))
+X_start = int(input('Enter starting point of x: '))
+Y_start = int(input('Enter starting point of y: '))
 
-value = gradient_decent(input_expr, learningRate, Iteration, X_Start, Y_Start)
-print(value)
+optimal_points = gradient_descent(input_expr, learning_rate, iteration, X_start, Y_start)
+print("Optimal points are:", optimal_points[:2], "and optimal value of the function is:", optimal_points[2])
